734ea78f21938213ddf3e27ba9d6aa38633a5ae3
github username <um_2003_8@hotmail.com>
2022-08-27 22:01:37 -0400

    create a method to convert random words to string

--- game/src/game/Words.java
+++ game/src/game/Words.java
@@ -3,4 +3,8 @@ package game;
 public class Words {
 	private String[] randomWords = {"animals", "happiness", "indefinite", "steady", "birthday", "extreme", "rights", "ceremony", "independence",
 									"beneath", "information", "students", "employee"};
+	
+	public String toString() {
+		return randomWords[1];
+	}
 }
