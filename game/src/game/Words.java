a812de77040f6b91a614d70f11433a4cf28d9c89
github username <um_2003_8@hotmail.com>
2022-08-30 12:17:42 -0400

    create a instance method random that shuffles a random words

--- game/src/game/Words.java
+++ game/src/game/Words.java
@@ -1,12 +1,15 @@
 package game;
 
+import java.util.Random;
+
 public class Words {
 	private String[] randomWords = {"animals", "happiness", "indefinite", "steady", "birthday", "extreme", "rights", "ceremony", "independence",
 									"beneath", "information", "students", "employee"};
 	private String selectedWord;
+	private Random random = new Random();
 	
 	public Words() {
-		selectedWord = randomWords[3];
+		selectedWord = randomWords[random.nextInt(randomWords.length)];
 	}
 	
 	public String toString() {
